b72a666451c8987cb91244f8c25aec153fc265c7
Seanpr <seanpr96@gmail.com>
2019-05-18 16:51:43 -0700

    Extension to mimic Enum.TryParse

---
+++ SeanprCore/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace SeanprCore
+{
+    [PublicAPI]
+    public static class StringExtensions
+    {
+        public static bool TryToEnum<T>(this string self, out T val) where T : Enum
+        {
+            try
+            {
+                val = (T)Enum.Parse(typeof(T), self);
+                return true;
+            }
+            catch
+            {
+                val = default;
+                return false;
+            }
+        }
+    }
+}
