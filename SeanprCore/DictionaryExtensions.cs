2fcc828d9ae8ffe1fe49c0b3153974aa65ca08c5
Seanpr <seanpr96@gmail.com>
2019-08-11 19:27:59 -0700

    Dictionary->Tuples extension

---
+++ SeanprCore/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace SeanprCore
+{
+    [PublicAPI]
+    public static class DictionaryExtensions
+    {
+        public static IEnumerable<(TKey, TVal)> AsTuples<TKey, TVal>(this IDictionary<TKey, TVal> self)
+        {
+            foreach (KeyValuePair<TKey, TVal> pair in self)
+            {
+                yield return (pair.Key, pair.Value);
+            }
+        }
+    }
+}
